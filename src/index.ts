514fdf6b743d4a59028b547389f07255077b9d5d
dmlyczynski <52506521+dmlyczynski@users.noreply.github.com>
2024-01-21 11:58:49 +0100

    Add ActionType

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export type ServiceYear = 2020 | 2021 | 2022;
 export type ServiceType = "Photography" | "VideoRecording" | "BlurayPackage" | "TwoDayEvent" | "WeddingSession";
+export type ActionType = "Select" | "Deselect";
 
 export const updateSelectedServices = (
     previouslySelectedServices: ServiceType[],
-    action: { type: "Select" | "Deselect"; service: ServiceType }
+    action: { type: ActionType; service: ServiceType }
 ) => [];
 
 export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => ({ basePrice: 0, finalPrice: 0 });
