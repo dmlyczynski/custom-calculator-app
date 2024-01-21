68bd26355d3bac1ecc3cd9add2330f5595079a05
dmlyczynski <52506521+dmlyczynski@users.noreply.github.com>
2024-01-21 12:14:46 +0100

    Handle if service exists

--- src/handleDeselect.ts
+++ src/handleDeselect.ts
@@ -1,5 +1,5 @@
 import { ServiceType } from ".";
 
-export const handleDeselect = (previouslySelectedServices: ServiceType[], service: string): ServiceType[] => {
+export const handleDeselect = (previouslySelectedServices: ServiceType[], service: ServiceType): ServiceType[] => {
     throw new Error("Function not implemented.");
 };
--- src/handleSelect.ts
+++ src/handleSelect.ts
@@ -1,5 +1,12 @@
 import { ServiceType } from ".";
 
-export const handleSelect = (previouslySelectedServices: ServiceType[], service: string): ServiceType[] => {
-    throw new Error("Function not implemented.");
+export const handleSelect = (previouslySelectedServices: ServiceType[], service: ServiceType): ServiceType[] => {
+    if (isServiceExistsInPreviouslySelectedServices(previouslySelectedServices, service)) {
+        return previouslySelectedServices;
+    }
+
+};
+
+export const isServiceExistsInPreviouslySelectedServices = (selectedServices: ServiceType[], service: ServiceType) => {
+    return selectedServices.includes(service);
 };
