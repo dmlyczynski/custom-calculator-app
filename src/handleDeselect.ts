6e586839064e92c4addbd43e40c152caeee7733e
dmlyczynski <52506521+dmlyczynski@users.noreply.github.com>
2024-01-21 12:54:42 +0100

    Implement handle deselect

--- src/handleDeselect.ts
+++ src/handleDeselect.ts
@@ -1,5 +1,37 @@
 import { ServiceType } from ".";
+import { isMainServices } from "./helpers";
 
-export const handleDeselect = (previouslySelectedServices: ServiceType[], service: ServiceType): ServiceType[] => {
-    throw new Error("Function not implemented.");
+export const handleDeselect = (selectedServices: ServiceType[], service: ServiceType): ServiceType[] => {
+    if (selectedServices.includes(service)) {
+        let updatedServices: ServiceType[] = selectedServices.filter((s) => s !== service);
+
+        if (isMainServices(updatedServices)) {
+            return updatedServices;
+        }
+
+        return updateRelatedServices(service, updatedServices);
+    }
+
+    return selectedServices;
+};
+
+const updateRelatedServices = (service: ServiceType, updatedServices: ServiceType[]) => {
+    const releatedServices = getRelatedServices(service);
+
+    if (releatedServices.length !== 0) {
+        updatedServices = updatedServices.filter((element) => !releatedServices.includes(element));
+    }
+    return updatedServices;
+}
+
+const getRelatedServices = (mainService: ServiceType): ServiceType[] => {
+    const relatedServices: Record<ServiceType, ServiceType[]> = {
+        Photography: ["TwoDayEvent"],
+        VideoRecording: [],
+        BlurayPackage: [],
+        TwoDayEvent: [],
+        WeddingSession: [],
+    };
+
+    return relatedServices[mainService] || [];
 };
--- src/helpers.ts
+++ src/helpers.ts
@@ -3,3 +3,7 @@ import { ServiceType } from ".";
 export const isMainService = (service: ServiceType): boolean => {
   return service === "Photography" || service === "VideoRecording";
 };
+
+export const isMainServices = (services: ServiceType[]): boolean => {
+  return services.some((service) => isMainService(service));
+};
